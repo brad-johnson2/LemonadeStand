30ac797b974129de2cf79614ba055e05cdad2ec6
brad-johnson2 <46530051+brad-johnson2@users.noreply.github.com>
2019-02-07 15:57:12 -0600

    Created Recipe class and added to Game

---
+++ Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LemonadeStand
+{
+    public class Recipe
+    {
+        public int lemonRecipe;
+        public int sugarRecipe;
+        public int iceRecipe;
+
+        public Recipe()
+        {
+
+        }
+        public int FindLemons()
+        {
+            Console.WriteLine("How many lemons per cup?");
+            lemonRecipe = int.Parse(Console.ReadLine());
+        }
+        public int FindSugar()
+        {
+            Console.WriteLine("How much sugar per cup?");
+            sugarRecipe = int.Parse(Console.ReadLine());
+        }
+        public int FindIce()
+        {
+            Console.WriteLine("How much ice per cup?");
+            iceRecipe = int.Parse(Console.ReadLine());
+        }
+
+
+
+    }
+}
